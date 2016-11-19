c30039ab8b83cbbb3192bc418ffed58a59505fc1
Alex Ogorek <ogoalex57@gmail.com>
2016-11-19 17:51:18 -0500

    parse added

--- src/pages/reviews/reviews.ts
+++ src/pages/reviews/reviews.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Parse } from 'parse';
 
 import { NavController } from 'ionic-angular';
 
@@ -8,7 +9,23 @@ import { NavController } from 'ionic-angular';
 })
 export class ReviewsPage {
 
+	 listOfThings = [];
+
   constructor(public navCtrl: NavController) {
+  	Parse.initialize("ASDFGHJKL"); 
+  	Parse.serverURL = 'https://hackohio.herokuapp.com/parse';
+
+  	let query = new Parse.Query("Building");
+  	query.find().then(function(results) {
+  		results.forEach(function(item) {
+  			this.listOfThings.push(item.get("Name"));
+  		});
+  	}, function(error) {
+  		alert("There was an error: " + error + JSON.stringify(error));
+  	});
+  }
+
+  textChanged(event) {
 
   }
 
