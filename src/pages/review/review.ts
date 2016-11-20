de3e007680160ecf4ea6d9959e27ccff5de3f720
Alex Ogorek <ogoalex57@gmail.com>
2016-11-20 18:00:14 -0500

    search working

--- src/pages/review/review.ts
+++ src/pages/review/review.ts
@@ -7,7 +7,9 @@ import { NavController } from 'ionic-angular';
 	templateUrl: 'review.html'
 })
 export class ReviewPage {
+	searchString = "";
 	listOfThings = [];
+	fullListOfBuildings = [];
 	constructor(public navCtrl: NavController) {
 		Parse.initialize("ASDFGHJKL"); 
 		Parse.serverURL = 'https://hackohio.herokuapp.com/parse';
@@ -19,7 +21,7 @@ export class ReviewPage {
 		let me = this; // needed for Parse query to access "this." variables
 		let query = new Parse.Query("Building");
 		query.find().then(function(results) {
-			me.listOfThings = [];
+			me.fullListOfBuildings = [];
 			results.forEach(function(item) {
 				// mapping Parse objects to our custom objects
 				let newItem = {
@@ -28,7 +30,9 @@ export class ReviewPage {
 					"totalRatings" : item.get("TotalRatings"),
 					"imageURL" : item.get("ImageURL")
 				};
-				me.listOfThings.push(newItem);
+				me.fullListOfBuildings.push(newItem);
+				me.listOfThings = me.fullListOfBuildings;
+				me.runSearch();
 			});
 		}, function(error) {
 			alert("There was an error: " + error + JSON.stringify(error));
@@ -36,6 +40,17 @@ export class ReviewPage {
 	}	
 
 	textChanged(event) {
-		
+		this.searchString = event.target.value;
+		this.runSearch();
+	}
+
+	runSearch() {
+		let me = this;
+		this.listOfThings = [];
+		this.fullListOfBuildings.forEach(function(item) {
+			if (item.name.toLowerCase().indexOf(me.searchString.toLowerCase()) !== -1) {
+				me.listOfThings.push(item);
+			}
+		});
 	}
 }
