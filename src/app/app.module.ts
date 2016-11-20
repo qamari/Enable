ddbbfe988b00f543ef05bce05bb792bd8f1d9030
Alice Li <Alice Li>
2016-11-20 11:07:50 -0500

    alice made changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { ReviewPage } from '../pages/review/review';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -10,7 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    ReviewPage,
     HomePage,
     TabsPage
   ],
@@ -21,7 +21,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    ReviewPage,
     HomePage,
     TabsPage
   ],
--- src/pages/review/review.ts
+++ src/pages/review/review.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-review',
+  templateUrl: 'review.html'
 })
-export class ContactPage {
+export class ReviewPage {
 
   constructor(public navCtrl: NavController) {
 
