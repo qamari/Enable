b83c657b80d87800c0ed9c800e323816e68a6c61
Iman Qamar <imanqamar@Imans-MacBook-Air.local>
2016-11-19 20:38:42 -0500

    reverted to working code

--- src/pages/reviews/reviews.ts
+++ src/pages/reviews/reviews.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Parse } from 'parse';
 
 import { NavController } from 'ionic-angular';
 
@@ -9,23 +8,7 @@ import { NavController } from 'ionic-angular';
 })
 export class ReviewsPage {
 
-	 listOfThings = [];
-
   constructor(public navCtrl: NavController) {
-  	Parse.initialize("ASDFGHJKL"); 
-  	Parse.serverURL = 'https://hackohio.herokuapp.com/parse';
-
-  	let query = new Parse.Query("Building");
-  	query.find().then(function(results) {
-  		results.forEach(function(item) {
-  			this.listOfThings.push(item.get("Name"));
-  		});
-  	}, function(error) {
-  		alert("There was an error: " + error + JSON.stringify(error));
-  	});
-  }
-
-  textChanged(event) {
 
   }
 
