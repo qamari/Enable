f138e7cd9907a9cc54821b39717a751006b77ad1
Alex Ogorek <ogoalex57@gmail.com>
2016-11-20 15:46:15 -0500

    parse working with list

--- src/pages/review/review.ts
+++ src/pages/review/review.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
-
+import { Parse } from 'parse';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-review',
-  templateUrl: 'review.html'
+	selector: 'page-review',
+	templateUrl: 'review.html'
 })
 export class ReviewPage {
+	listOfThings = [];
+	constructor(public navCtrl: NavController) {
+		Parse.initialize("ASDFGHJKL"); 
+		Parse.serverURL = 'https://hackohio.herokuapp.com/parse';
 
-  constructor(public navCtrl: NavController) {
+		this.refreshBuildings();
+	}
 
-  }
-
+	refreshBuildings() {
+		let me = this; // needed for Parse query to access "this." variables
+		let query = new Parse.Query("Building");
+		query.find().then(function(results) {
+			me.listOfThings = [];
+			results.forEach(function(item) {
+				me.listOfThings.push(item.get("Name"));
+			});
+		}, function(error) {
+			alert("There was an error: " + error + JSON.stringify(error));
+		});
+	}	
 }
